7af7333a39dc9ab84f13424c757585d9f8655bff
Jorge Moreno <jorge.moreno.fernandez@outlook.com>
2025-01-08 20:06:12 +0100

    feat: add GetSteamAvatar async action

---
+++ Plugins/SteamDomeFriends/Source/SteamDomeFriends/Private/AsyncActions/GetSteamAvatarAsyncAction.cpp
@@ -0,0 +1,163 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AsyncActions/GetSteamAvatarAsyncAction.h"
+
+#include <steam/isteamutils.h>
+
+#include "Enums/SteamPersonaChange.h"
+#include "Subsystems/SteamDomeFriendsSubsystem.h"
+
+
+UGetSteamAvatarAsyncAction* UGetSteamAvatarAsyncAction::GetSteamAvatar(
+	const UObject* WorldContextObject, const FSteamId SteamId, const ESteamAvatarSize AvatarSize
+) {
+	auto* Instance = NewObject<UGetSteamAvatarAsyncAction>();
+	Instance->WorldContext = WorldContextObject;
+	Instance->UserSteamId = SteamId;
+	Instance->Size = AvatarSize;
+
+	Instance->RegisterWithGameInstance(WorldContextObject);
+
+	return Instance;
+}
+
+
+void UGetSteamAvatarAsyncAction::Activate()
+{
+	Super::Activate();
+
+	const int32 ImageIndex = TryGetLoadedAvatarPictureIndex();
+
+	if (ImageIndex > 0)
+	{
+		// Image can be loaded normally
+		auto* AvatarPicture = GetAvatarFromSteamCache(ImageIndex);
+		BroadcastAvatar(AvatarPicture);
+		return;
+	}
+
+	// Handle image not being ready in cache
+	
+	if (ImageIndex == 0)
+	{
+		// Avatar is not set, we need to manually request it
+		SteamDomeFriends = USteamDomeFriendsSubsystem::Get(WorldContext);
+		DelegateHandle = SteamDomeFriends->OnPersonaStateChange.AddUObject(this, &UGetSteamAvatarAsyncAction::OnPersonaStateChange);
+		const bool bSuccess = SteamDomeFriends->RequestUserInformation(UserSteamId.GetCSteamID(), false);
+
+		if (!bSuccess)
+		{
+			StopWithError();
+		}
+	}
+	else
+	{
+		// Avatar is automatically requested by Steamworks,
+		// we just need to wait for the OnAvatarLoaded_t
+		SteamDomeFriends = USteamDomeFriendsSubsystem::Get(WorldContext);
+		DelegateHandle = SteamDomeFriends->OnAvatarImageLoaded.AddUObject(this, &UGetSteamAvatarAsyncAction::OnImageLoaded);
+	}
+}
+
+
+UTexture2D* UGetSteamAvatarAsyncAction::GetAvatarFromSteamCache(const int32 PictureId, uint32 Width, uint32 Height)
+{
+	bool bSuccess = SteamUtils()->GetImageSize(PictureId, &Width, &Height);
+	
+	if (!bSuccess)
+	{
+		return nullptr;
+	}
+
+	constexpr int32 NumChannels = 4; // 4 channels = RGBA
+	TArray<uint8> AvatarRGBA = TArray<uint8>();
+	AvatarRGBA.SetNumUninitialized(Width * Height * NumChannels);
+	
+	bSuccess = SteamUtils()->GetImageRGBA(
+		PictureId, AvatarRGBA.GetData(), AvatarRGBA.Num()
+	);
+	
+	if (!bSuccess)
+	{
+		return nullptr;
+	}
+
+	return UTexture2D::CreateTransient(Width, Height, PF_R8G8B8A8, NAME_None, AvatarRGBA);
+}
+
+
+UTexture2D* UGetSteamAvatarAsyncAction::GetAvatarFromSteamCache(const int32 PictureId)
+{
+	uint32 Width, Height;
+	const bool bSuccess = SteamUtils()->GetImageSize(PictureId, &Width, &Height);
+		
+	if (!bSuccess)
+	{
+		return nullptr;
+	}
+
+	return GetAvatarFromSteamCache(PictureId, Width, Height);
+}
+
+
+void UGetSteamAvatarAsyncAction::OnPersonaStateChange(const FSteamId SteamId, const int32 PersonaFlags)
+{
+	if (SteamId == UserSteamId && PersonaChange::HasBitEnabled(PersonaFlags, ESteamPersonaChange::Avatar))
+	{
+		SteamDomeFriends->OnPersonaStateChange.Remove(DelegateHandle);
+		
+		const int32 ImageIndex = TryGetLoadedAvatarPictureIndex();
+		if (ImageIndex <= 0)
+		{
+			StopWithError();
+			return;
+		}
+
+		auto* AvatarPicture = GetAvatarFromSteamCache(ImageIndex);
+		BroadcastAvatar(AvatarPicture);
+	}
+}
+
+
+void UGetSteamAvatarAsyncAction::OnImageLoaded(
+	const FSteamId SteamId, const int32 PictureId, const int32 Width, const int32 Height
+) {
+	if (SteamId == UserSteamId)
+	{
+		SteamDomeFriends->OnAvatarImageLoaded.Remove(DelegateHandle);
+		
+		auto* AvatarPicture = GetAvatarFromSteamCache(PictureId, Width, Height);
+		BroadcastAvatar(AvatarPicture);
+	}
+}
+
+
+int32 UGetSteamAvatarAsyncAction::TryGetLoadedAvatarPictureIndex()
+{
+	switch (Size) {
+	case ESteamAvatarSize::Small:
+		return USteamDomeFriendsSubsystem::GetSmallFriendAvatar(UserSteamId);
+	case ESteamAvatarSize::Medium:
+		return USteamDomeFriendsSubsystem::GetMediumFriendAvatar(UserSteamId);
+	case ESteamAvatarSize::Large:
+		return USteamDomeFriendsSubsystem::GetLargeFriendAvatar(UserSteamId);
+	}
+
+	checkNoEntry();
+	return -2;
+}
+
+
+void UGetSteamAvatarAsyncAction::BroadcastAvatar(UTexture2D* Texture)
+{
+	OnFinish.Broadcast(Texture);
+	SetReadyToDestroy();
+}
+
+
+void UGetSteamAvatarAsyncAction::StopWithError()
+{
+	OnFinish.Broadcast(nullptr);
+	SetReadyToDestroy();
+}
---
+++ Plugins/SteamDomeFriends/Source/SteamDomeFriends/Public/AsyncActions/GetSteamAvatarAsyncAction.h
@@ -0,0 +1,97 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Structs/SteamId.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "GetSteamAvatarAsyncAction.generated.h"
+
+
+class USteamDomeFriendsSubsystem;
+
+
+/**
+ * The sizes of the Steam avatar to load.
+ */
+UENUM(BlueprintType)
+enum class ESteamAvatarSize : uint8
+{
+	Small,	// 32x32 pixels
+	Medium, // 64x64 pixels
+	Large,	// 128x128 pixels
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FLoadAvatarFinishSignature, UTexture2D*, Avatar);
+
+/**
+ * 
+ */
+UCLASS()
+class STEAMDOMEFRIENDS_API UGetSteamAvatarAsyncAction : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	TObjectPtr<const UObject> WorldContext;
+
+	UPROPERTY()
+	FSteamId UserSteamId;
+
+	UPROPERTY()
+	ESteamAvatarSize Size;
+
+	/* Common between the two types of async loading (they can't happen at the same time) */
+	FDelegateHandle DelegateHandle;
+	
+	TObjectPtr<USteamDomeFriendsSubsystem> SteamDomeFriends;
+
+public:
+
+	UPROPERTY(BlueprintAssignable)
+	FLoadAvatarFinishSignature OnFinish;
+	
+	/**
+	 * Retrieves or requests the avatar image for a given SteamId user.
+	 *
+	 * If the image could not be retrieved, returns an invalid Texture2D.
+	 *
+	 * @note Requesting the large size, could mean waiting more for the image,
+	 *		 as large avatars are not automatically downloaded by Steam.
+	 * 
+	 * @param WorldContextObject 
+	 * @param SteamId SteamId for the user.
+	 * @param AvatarSize The size of the requested image.
+	 */
+	UFUNCTION(BlueprintCallable, meta=(BlueprintInternalUseOnly="true", WorldContext="WorldContextObject"))
+	static UGetSteamAvatarAsyncAction* GetSteamAvatar(
+		const UObject* WorldContextObject, const FSteamId SteamId, const ESteamAvatarSize AvatarSize
+	);
+
+	virtual void Activate() override;
+
+	/**
+	 * TODO: Move to the SteamUtils when available.
+	 * Returns an Texture2D containing the SteamId avatar if the PictureId is valid.
+	 *
+	 * @param PictureId 
+	 * @param Width Optional, if not set, the PictureId width is retrieved from Steamworks API
+	 * @param Height Optional, if not set, the PictureId height is retrieved from Steamworks API
+	 * @return Texture2D containing the SteamId avatar if the PictureId is valid, nullptr otherwise 
+	 */
+	static UTexture2D* GetAvatarFromSteamCache(const int32 PictureId, uint32 Width, uint32 Height);
+	static UTexture2D* GetAvatarFromSteamCache(const int32 PictureId);
+
+protected:
+
+	UFUNCTION()
+	void OnPersonaStateChange(const FSteamId SteamId, const int32 PersonaFlags);
+
+	UFUNCTION()
+	void OnImageLoaded(const FSteamId SteamId, const int32 PictureId, const int32 Width, const int32 Height);
+
+	int32 TryGetLoadedAvatarPictureIndex();
+
+	void BroadcastAvatar(UTexture2D* Texture);
+	void StopWithError();
+};
