40577285ea4727758673ecbe72d67fd45bfc64e6
Jorge Moreno <jorge.moreno.fernandez@outlook.com>
2025-01-09 16:56:28 +0100

    feat: add IsFollowing async action

---
+++ Plugins/SteamDomeFriends/Source/SteamDomeFriends/Private/AsyncActions/IsFollowingAsyncAction.cpp
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AsyncActions/IsFollowingAsyncAction.h"
+
+#include "Subsystems/SteamDomeFriendsSubsystem.h"
+
+
+UIsFollowingAsyncAction* UIsFollowingAsyncAction::IsFollowing(const UObject* WorldContextObject, const FSteamId SteamId)
+{
+	auto* Instance = NewObject<UIsFollowingAsyncAction>();
+	Instance->WorldContext = WorldContextObject;
+	Instance->SteamId = SteamId;
+
+	Instance->RegisterWithGameInstance(WorldContextObject);
+
+	return Instance;
+}
+
+
+void UIsFollowingAsyncAction::Activate()
+{
+	Super::Activate();
+
+	IsFollowingCallback.Init(
+		FIsFollowingCallbackDelegate::CreateUObject(this, &ThisClass::OnIsFollowingCompleted)
+	);
+
+	const auto Handle = SteamFriends()->IsFollowing(SteamId.GetCSteamID());
+	
+}
+
+
+void UIsFollowingAsyncAction::OnIsFollowingCompleted(FriendsIsFollowing_t* FriendsIsFollowing, const bool bIOFailure)
+{
+	if (bIOFailure || !FriendsIsFollowing || FriendsIsFollowing->m_eResult != k_EResultOK)
+	{
+		OnCompleted.Broadcast(false, false);
+	}
+	else
+	{
+		OnCompleted.Broadcast(true, FriendsIsFollowing->m_bIsFollowing);
+	}
+
+	SetReadyToDestroy();
+}
---
+++ Plugins/SteamDomeFriends/Source/SteamDomeFriends/Public/AsyncActions/IsFollowingAsyncAction.h
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Structs/SteamId.h"
+#include <steam/isteamfriends.h>
+#include "Helpers/SteamCallResult.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "IsFollowingAsyncAction.generated.h"
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(
+	FIsFollowingCompletedSignature,
+	const bool, bSuccess,
+	const bool, bIsFollowing
+);
+
+/**
+ * 
+ */
+UCLASS()
+class STEAMDOMEFRIENDS_API UIsFollowingAsyncAction : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+	STEAMDOME_CALLRESULT(FIsFollowingCallback, FriendsIsFollowing_t)
+	FIsFollowingCallback IsFollowingCallback;
+
+	UPROPERTY()
+	TObjectPtr<const UObject> WorldContext;
+
+	FSteamId SteamId;
+
+public:
+	
+	UPROPERTY(BlueprintAssignable)
+	FIsFollowingCompletedSignature OnCompleted;
+
+
+	/**
+	 * Checks if the current user is following the specified user.
+	 * 
+	 * @param WorldContextObject 
+	 * @param SteamId Target SteamId.
+	 * @return 
+	 */
+	UFUNCTION(BlueprintCallable, meta=(BlueprintInternalUseOnly="true", WorldContext="WorldContextObject"))
+	static UIsFollowingAsyncAction* IsFollowing(
+		const UObject* WorldContextObject, const FSteamId SteamId
+	);
+
+	virtual void Activate() override;
+
+protected:
+
+	void OnIsFollowingCompleted(FriendsIsFollowing_t* FriendsIsFollowing, const bool bIOFailure);
+};
